f9832a10fbaa76ecd5b371adbaea8d511acbf6ef
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2024-09-02 15:16:12 +0800

    Initial Commit

---
+++ SoulFoodApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SoulFoodApp
+//
+//  Created by Cheong Jia Zheng on 2/9/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
