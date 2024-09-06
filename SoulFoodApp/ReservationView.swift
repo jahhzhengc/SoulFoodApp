7c6d793e7b4532488ffa0a35510c46a5bfb7f649
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2024-09-06 18:11:32 +0800

    Managed to limit the time and date properly for DatePicker

--- SoulFoodApp/ReservationView.swift
+++ SoulFoodApp/ReservationView.swift
@@ -9,17 +9,47 @@ import SwiftUI
 
 struct ReservationView: View {
     let tomorrow = Date.now.addingTimeInterval(60 * 60 * 24)
+    let nextMonth = Date.now.addingTimeInterval(60 * 60 * 24 * 30)
+
     @State private var wakeUp = Date.now
     let dateFormatter = DateFormatter()
                                                 // 60s * 60mins * 24hrs * 30days LOL
-    let nextMonth = Date.now.addingTimeInterval(60 * 60 * 24 * 30)
+    
+    @State var favourited : Bool = false
+    
+    var time: ClosedRange<Date> {
+//        _start =
+        return tomorrow...nextMonth
+    }
+    @State private var selectedTime = Date()
+
+    // limit the hours for time picker
+    var getTime: some View {
+           let calendar = Calendar.current
+           let startDate = calendar.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
+           let endDate = calendar.date(bySettingHour: 20, minute: 0, second: 0, of: Date())!
+           
+           return DatePicker("Select Time", selection: $selectedTime,
+                             in: startDate...endDate,
+                             displayedComponents: .hourAndMinute)
+               .datePickerStyle(CompactDatePickerStyle())
+               .environment(\.locale, Locale(identifier: "en_US_POSIX"))
+               .labelsHidden()
+       }
+    
+    
     var body: some View {
-        
-        DatePicker("Pick a date", selection : $wakeUp, in: tomorrow...nextMonth)
-            .onAppear(perform:{
+        // limits the date, to tomorrow ~ 1 month after
+        DatePicker("Pick a date", selection : $wakeUp, in: time, displayedComponents: .date)
+            .onAppear(perform:{ 
+                UIDatePicker.appearance().minuteInterval = 15
                 dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
             })
-        // by right, the timepicker should be given certain timing after getting the 'available' reservation 
+            .labelsHidden()
+        getTime
+        Text(wakeUp.description)
+        
+        // by right, the timepicker should be given certain timing after getting the 'available' reservation
     }
 }
 
